26fbae3d8e67c79efb8ba795dbe22cc407055757
unknown <tanish@ua.i>
2024-06-09 20:37:32 +0300

    up

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,6 +1,3 @@
 import path from 'node:path';
-import fs from 'node:fs/promises';
 
-export const PATH_DB = path.join(process.cwd(), 'src', 'db', 'db.json');
-const dbJson = await fs.readFile(PATH_DB, 'utf8');
-export const DB_DATA = JSON.parse(dbJson);
+export const PATH_DB = path.join('src', 'db', 'db.json');
